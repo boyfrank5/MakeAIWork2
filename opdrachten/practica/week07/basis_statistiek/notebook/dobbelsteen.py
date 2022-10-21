8fac1980c8eea102c430aa89801208720bfac6ec
boyfrank5 <boyclaesen@gmail.com>
2022-10-21 16:07:56 +0200

    Afronding notebooks periode 2

--- opdrachten/practica/week07/basis_statistiek/notebook/dobbelsteen.py
+++ opdrachten/practica/week07/basis_statistiek/notebook/dobbelsteen.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 import random
+import numpy as np
+
 
 class Dobbelsteen:
     
     def __init__(self):
+        self.history = list()
         self.values = set(range(1, 7))  
         self.roll()
         
@@ -74,17 +77,26 @@ class Dobbelsteen:
 
     def roll(self):
         self.number = random.choice(self.getList())
+        self.history.append(self.number)
 
     def getNumber(self):
         return self.number
 
     def show(self):        
         return str(self.faces.get(self.number))
-
-def main():
+    
+    def getHistory(self):
+        return np.array(self.history) # Geef de variabele 'history' als een numpy array terug.
+    
+    
+def main(): #conventie van python om altijd een Main functie te begruiken. 
     d = Dobbelsteen()
     d.roll()
     d.show()
+    d.getHistory() 
+    print(d.getHistory())
 
+
+    
 if __name__ == main():
-    main()                
+    main()           
